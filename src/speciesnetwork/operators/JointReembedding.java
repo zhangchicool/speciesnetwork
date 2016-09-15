1b0356102827aa16144af34e4613c07d27a22c0e
Chi Zhang <zhangchicool@gmail.com>
2016-09-15 10:24:33 +0200

    JointReembedding is able to rebuild embeddings of multiple gene trees

--- src/speciesnetwork/operators/JointReembedding.java
+++ src/speciesnetwork/operators/JointReembedding.java
@@ -14,10 +14,10 @@ import beast.core.StateNode;
 
 @Description("Combine a gene tree operator with RebuildEmbedding.")
 public class JointReembedding extends Operator {
-    public Input<RebuildEmbedding> rebuildEmbeddingInput = new Input<>("rebuildEmbedding",
-            "Operator which rebuilds embedding of gene tree within species network.", Validate.REQUIRED);
     public Input<Operator> treeOperatorInput = new Input<>("operator",
             "Tree operator to combine into RebuildEmbedding.", Validate.REQUIRED);
+    public Input<List<RebuildEmbedding>> rebuildEmbeddingInput = new Input<>("rebuildEmbedding",
+            "Operator which rebuilds embedding of gene tree within species network.", new ArrayList<>());
 
     @Override
     public void initAndValidate() {
@@ -25,32 +25,36 @@ public class JointReembedding extends Operator {
 
     @Override
     public double proposal() {
-        RebuildEmbedding reembedOp = rebuildEmbeddingInput.get();
         Operator treeOp = treeOperatorInput.get();
-        
+        List<RebuildEmbedding> reembedOps = rebuildEmbeddingInput.get();
+
         // count the number of alternative traversing choices for the current state (n0)
-        final int oldChoices = reembedOp.getNumberOfChoices();
-        if (oldChoices < 0)
-            throw new RuntimeException("Developer ERROR: current embedding invalid!");
+        int oldChoices = 0;
+        for (RebuildEmbedding reembedOp: reembedOps) {
+            final int nChoices = reembedOp.getNumberOfChoices();
+            if (nChoices < 0)
+                throw new RuntimeException("Developer ERROR: current embedding invalid!");
+            oldChoices += nChoices;
+        }
 
         // first make the operation
         double logHR = treeOp.proposal();
         if (logHR == Double.NEGATIVE_INFINITY)
             return Double.NEGATIVE_INFINITY;
-
         // Update calculation nodes as subsequent operators may depend on state nodes made dirty by this operation.
         if (!treeOp.listStateNodes().isEmpty())  // copied from JointOperator
             treeOp.listStateNodes().get(0).getState().checkCalculationNodesDirtiness();
 
         // then rebuild the embedding
-        final int newChoices = reembedOp.initializeEmbedding();
-        if (newChoices < 0)
-            return Double.NEGATIVE_INFINITY;
-
-        // Update calculation nodes as subsequent operators may depend on state nodes made dirty by this operation.
-        if (!reembedOp.listStateNodes().isEmpty()) // copied from JointOperator
-            reembedOp.listStateNodes().get(0).getState().checkCalculationNodesDirtiness();
-
+        int newChoices = 0;
+        for (RebuildEmbedding reembedOp: reembedOps) {
+            final int nChoices = reembedOp.initializeEmbedding();
+            if (nChoices < 0)
+                return Double.NEGATIVE_INFINITY;
+            newChoices += nChoices;
+            if (!reembedOp.listStateNodes().isEmpty()) // copied from JointOperator
+                reembedOp.listStateNodes().get(0).getState().checkCalculationNodesDirtiness();
+        }
 
         return logHR + (newChoices - oldChoices) * Math.log(2);
     }
@@ -58,9 +62,11 @@ public class JointReembedding extends Operator {
     @Override
     public List<StateNode> listStateNodes() {
         List<StateNode> stateNodeList = new ArrayList<>();
+        List<RebuildEmbedding> reembedOps = rebuildEmbeddingInput.get();
 
         stateNodeList.addAll(treeOperatorInput.get().listStateNodes());
-        stateNodeList.addAll(rebuildEmbeddingInput.get().listStateNodes());
+        for (RebuildEmbedding reembedOp: reembedOps)
+            stateNodeList.addAll(reembedOp.listStateNodes());
 
         return stateNodeList;
     }
