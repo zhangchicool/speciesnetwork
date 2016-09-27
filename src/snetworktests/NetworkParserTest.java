038e80cf65754ec95473dbf79058ce6bd5803013
Chi Zhang <zhangchicool@gmail.com>
2016-09-27 17:29:17 +0200

    why not parsing???

--- src/snetworktests/NetworkParserTest.java
+++ src/snetworktests/NetworkParserTest.java
@@ -10,12 +10,10 @@ public class NetworkParserTest {
     TreeParser treeParser;
     NetworkParser networkParser;
 
-    // "Partial" reticulation nodes (that is, #H nodes without any directly attached children in the extended newick string)
-    // should be on the right. "Complete" reticulation nodes (with directly attached children) should be on the left.
-
-    final String testNetwork1 = "(((A:0.2,(B:0.1)#H1[&gamma=0.9]:0.1)S1:0.3,(C:0.3,#H1:0.2)S2:0.2)R:0.1)";
-
-    final String testNetwork = "(((((A:0.1)#H1[&gamma=0.9]:0.1)#H2[&gamma=0.8]:0.3,((#H2:0.1,(#H1:0.1)#H3[&gamma=0.7]:0.1)S1:0.1)#H4[&gamma=0.6]:0.1)S2:0.1,(#H4:0.1,#H3:0.3)S3:0.1)R:0.1)";
+    final String testNetwork1 = "(((A:0.2,(B:0.1)#H1[&gamma=0.9]:0.1)S1:0.3,(#H1:0.2,C:0.3)S2:0.2)R:0.1)";
+    final String testNetwork2 = "(((((A:0.1)#H1[&gamma=0.9]:0.1)#H2[&gamma=0.8]:0.3,((#H2:0.1,(#H1:0.1)#H3[&gamma=0.7]:0.1)S1:0.1)#H4[&gamma=0.6]:0.1)S2:0.1,(#H4:0.1,#H3:0.3)S3:0.1)R:0.1)";
+    //why???
+    final String testNetwork = "((((A:0.02,#H1:0.01):0.01,#H2:0.01):0.02,(((B:0.01)#H1:0.01)#H2:0.02,C:0.04):0.01)R:0.01)";
 
     public NetworkParserTest() {
         treeParser = new TreeParser();
