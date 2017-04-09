c02f70956d9d22eec8ecaab18acb13fd612667f9
Huw A. Ogilvie <huw.ogilvie@anu.edu.au>
2017-04-09 14:55:49 +1000

    Working on summarizing heights and gammas

--- src/speciesnetwork/tools/SummarizePosterior.java
+++ src/speciesnetwork/tools/SummarizePosterior.java
@@ -1,12 +1,15 @@
 package speciesnetwork.tools;
 
 import java.io.*;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Table;
 
@@ -20,7 +23,7 @@ import speciesnetwork.NetworkNode;
 import speciesnetwork.NetworkParser;
 
 /**
- * @author Huw A. Ogilvie
+ * @author Huw Ogilvie
  */
 
 @Description("Summarize the posterior distribution of species networks.")
@@ -38,8 +41,8 @@ public class SummarizePosterior extends Runnable {
 
     @Override
     public void initAndValidate() {
-    	rSubnetworks = new HashMap<>();
-    	sSubnetworks = HashBasedTable.create();
+    	rSubnetworks = new HashMap<>(); // subnetworks defined by a reticulation node
+    	sSubnetworks = HashBasedTable.create(); // subnetworks defined by a speciation node
     }
 
     @Override
@@ -66,19 +69,40 @@ public class SummarizePosterior extends Runnable {
         List<Tree> parsedTrees = nexusParser.trees;
 
         nextSubnetworkNumber = nexusParser.taxa.size();
-        Multiset<Integer> networkCounter = HashMultiset.create();
+        final Multiset<Integer> networkCounter = HashMultiset.create();
+        final Map<Integer, Multimap<Integer, Double>> networkHeights = new HashMap<>();
+        final Map<Integer, Multimap<Integer, Double>> networkGammas = new HashMap<>();
+        final Map<Integer, Network> uniqueNetworks = new HashMap<>();
+
         for (int i = 0; i < parsedTrees.size(); i++) {
         	if (i >= burnin) {
-        		Tree tree = parsedTrees.get(i);
-        		Network network = new NetworkParser(tree);
-        		NetworkNode root = network.getRoot();
-        		final int networkNr = findSubnetworks(root);
+        		final Tree tree = parsedTrees.get(i);
+        		final Network network = new NetworkParser(tree);
+        		final NetworkNode root = network.getRoot();
+                final Multimap<Integer, Double> sampleHeights = HashMultimap.create();
+                final Multimap<Integer, Double> sampleGammas = HashMultimap.create();
+        		final int networkNr = findSubnetworks(root, sampleHeights, sampleGammas, false);
         		networkCounter.add(networkNr);
         		System.out.println(String.format("Sample %d = network %d", i, networkNr));
+
+        		if (networkHeights.containsKey(networkNr)) {
+        			networkHeights.get(networkNr).putAll(sampleHeights);
+        			networkGammas.get(networkNr).putAll(sampleGammas);
+        		} else {
+        			networkHeights.put(networkNr, sampleHeights);
+        			networkGammas.put(networkNr, sampleGammas);
+        			uniqueNetworks.put(networkNr, network);
+        		}
         	}
         }
 
         for (Integer networkNr: networkCounter.elementSet()) {
+        	final Network example = uniqueNetworks.get(networkNr);
+        	final NetworkNode root = example.getRoot();
+        	final Multimap<Integer, Double> heights = networkHeights.get(networkNr);
+        	final Multimap<Integer, Double> gammas = networkGammas.get(networkNr);
+
+        	findSubnetworks(root, heights, gammas, true);
         	System.out.println(String.format("Network %d = %d counts", networkNr, networkCounter.count(networkNr)));
         }
     }
@@ -87,7 +111,7 @@ public class SummarizePosterior extends Runnable {
      * and whether it already has an assigned number.
      * If not, assign nextSubnetworkNumber.
      */
-    private Integer findSubnetworks(NetworkNode node) {
+    private Integer findSubnetworks(NetworkNode node, Multimap<Integer, Double> heights, Multimap<Integer, Double> gammas, boolean summarize) {
     	Multiset<NetworkNode> children = node.getChildren();
 
     	Integer subnetworkNr = -1;
@@ -95,7 +119,7 @@ public class SummarizePosterior extends Runnable {
     		subnetworkNr = node.getNr();
     	} else if (children.size() == 1) { // reticulation node
     		for (NetworkNode child: children) { // only runs once
-    			final int childSubnetworkNr = findSubnetworks(child);
+    			final int childSubnetworkNr = findSubnetworks(child, heights, gammas, summarize);
     			subnetworkNr = rSubnetworks.get(childSubnetworkNr);
 	    		if (subnetworkNr == null) {
 	    			subnetworkNr = nextSubnetworkNumber;
@@ -107,22 +131,39 @@ public class SummarizePosterior extends Runnable {
     		int leftSubnetworkNr = -1;
     		int rightSubnetworkNr = -1;
     		for (NetworkNode child: children) { // only runs twice
-    			final int childSubnetwork = findSubnetworks(child);
-    			if (leftSubnetworkNr == -1)
+    			final int childSubnetwork = findSubnetworks(child, heights, gammas, summarize);
+    			// "left" subnetwork is always the larger number
+    			if (leftSubnetworkNr < childSubnetwork) {
+    				rightSubnetworkNr = leftSubnetworkNr;
     				leftSubnetworkNr = childSubnetwork;
-    			else
+    			} else {
     				rightSubnetworkNr = childSubnetwork;
+    			}
     		}
 
     		subnetworkNr = sSubnetworks.get(leftSubnetworkNr, rightSubnetworkNr);
     		if (subnetworkNr == null) {
     			subnetworkNr = nextSubnetworkNumber;
     			sSubnetworks.put(leftSubnetworkNr, rightSubnetworkNr, subnetworkNr);
-    			sSubnetworks.put(rightSubnetworkNr, leftSubnetworkNr, subnetworkNr);
     			nextSubnetworkNumber++;
-    		} 
+    		}
+    	}
+
+    	if (summarize) {
+    		node.setHeight(calculateMean(heights.get(subnetworkNr)));
+    		node.setGammaProb(calculateMean(gammas.get(subnetworkNr)));
+    	} else {
+	    	heights.put(subnetworkNr, node.getHeight());
+	    	gammas.put(subnetworkNr, node.getGammaProb());
     	}
 
     	return subnetworkNr;
+    }
+
+    private double calculateMean(Collection<Double> collection) {
+    	double sumValues = 0.0;
+    	for (Double v: collection) sumValues += v;
+
+    	return sumValues / collection.size();
     }
 }
