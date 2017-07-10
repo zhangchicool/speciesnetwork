2b59c52da7da64e5e4c24331905b4b5f46a87a53
Chi Zhang <zhangchicool@gmail.com>
2017-07-10 10:13:42 +0200

    for my own purpose

---
+++ src/speciesnetwork/tools/ExtNexusParser.java
@@ -0,0 +1,58 @@
+package speciesnetwork.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import beast.util.NexusParserListener;
+import beast.util.TreeParser;
+import beast.util.NexusParser;
+
+/**
+ * parses nexus file and grabs alignment and calibration from the file
+ * Modified parseTreesBlock from NexusParser built into BEAST2 to support networks
+ */
+public class ExtNexusParser extends NexusParser {
+
+    @Override
+    protected void parseTreesBlock(final BufferedReader fin) throws IOException {
+        trees = new ArrayList<>();
+        // read to first non-empty line within trees block
+        String str = readLine(fin).trim();
+        while (str.equals("")) {
+            str = readLine(fin).trim();
+        }
+
+        int origin;
+        // if first non-empty line is "translate" then parse translate block
+        if (str.toLowerCase().contains("translate")) {
+            translationMap = parseTranslateBlock(fin);
+            origin = getIndexedTranslationMapOrigin(translationMap);
+            if (origin != -1) {
+                taxa = getIndexedTranslationMap(translationMap, origin);
+            }
+        }
+
+        // read trees
+        while (str != null) {
+            if (str.toLowerCase().startsWith("tree ")) {
+                final int i = str.indexOf('(');
+                if (i > 0) {
+                    str = str.substring(i);
+                }
+                final TreeParser treeParser = new TreeParser(str);  // modified here
+
+                if (translationMap != null) treeParser.translateLeafIds(translationMap);
+
+                // this needs to go after translation map or listeners have an incomplete tree!
+                for (final NexusParserListener listener : listeners) {
+                    listener.treeParsed(trees.size(), treeParser);
+                }
+
+                // this must come after listener or trees.size() gives the wrong index to treeParsed
+                trees.add(treeParser);
+            }
+            str = fin.readLine();
+            if (str != null) str = str.trim();
+        }
+    }
+}
--- src/speciesnetwork/tools/SummarizeNetwork.java
+++ src/speciesnetwork/tools/SummarizeNetwork.java
@@ -1,29 +1,21 @@
 package speciesnetwork.tools;
 
 import java.io.*;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
+import java.util.*;
 
 import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
+import beast.core.util.Log;
 import beast.core.Runnable;
+import beast.util.HeapSort;
 import beast.util.TreeParser;
 import speciesnetwork.Network;
 import speciesnetwork.NetworkNode;
 import speciesnetwork.NetworkParser;
-import speciesnetwork.NodeHeightComparator;
 
-/**
- * @author Chi Zhang
- */
-
-@Description("Summarize the posterior distribution of species networks.")
 @Deprecated
 public class SummarizeNetwork extends Runnable {
     public final Input<String> inputFileNameInput = new Input<>("inputFileName",
@@ -31,35 +23,50 @@ public class SummarizeNetwork extends Runnable {
     public final Input<String> outputFileNameInput = new Input<>("outputFileName",
             "If provided, write to this file rather than to standard out.");
     public final Input<Integer> burninInput = new Input<>("burnin", "The absolute burn-in.", 0);
+    public final Input<Boolean> medianInput = new Input<>("useMedian",
+            "Use median instead of mean for node heights and gamma probs in summary networks.", false);
+    public final Input<Integer> decimalPlacesInput = new Input<>("dp",
+            "The number of decimal places to use (default -1 for full precision)", -1);
 
-    // map the number of reticulations with the networks
-    private Multimap<Integer, Network> nHybridInNetworkMap = HashMultimap.create();
-
-    private static Comparator<NetworkNode> hc = new NodeHeightComparator();
+    private boolean useMedian;
+    private DecimalFormat df;
+    private static PrintStream progressStream = Log.info;
 
     @Override
     public void initAndValidate() {
+        useMedian = medianInput.get();
+
+        int dp = decimalPlacesInput.get();
+        if (dp < 0) {
+            df = null;
+        } else {
+            // just new DecimalFormat("#.######") (with dp time '#' after the decimal)
+            df = new DecimalFormat("#." + new String(new char[dp]).replace('\0', '#'));
+            df.setRoundingMode(RoundingMode.HALF_UP);
+        }
     }
 
     @Override
     public void run() throws IOException {
         final String inputFileName = inputFileNameInput.get();
         final String outputFileName = outputFileNameInput.get();
-
         PrintStream out;  // where to print
         if (outputFileName == null) {
             out = System.out;
         } else {
-            String msg = "Writing";
+            String msg = "Writing to";
             if (new File(outputFileName).exists())
                 msg = "Warning: Overwriting";
-            System.err.println(msg + " file " + outputFileName);
+            progressStream.println(msg + " file " + outputFileName);
             out = new PrintStream(outputFileName);
         }
-        // print header
-        out.println("nHybrid  length  height  tHybrid  gamma");
 
+        // get the absolute burn-in
         final int burnin = burninInput.get();
+
+        progressStream.print("Parsing network samples ");
+        // print header
+        out.println("length  height  nHybrid  tHybrid");
         int numNetworks = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
             String line;
@@ -70,61 +77,28 @@ public class SummarizeNetwork extends Runnable {
                     if (i > 0) line = line.substring(i);
                     TreeParser tree = new TreeParser(line);
                     NetworkParser network = new NetworkParser(tree);
-
                     numNetworks++;
-                    if (numNetworks > burnin)
-                        printSummary(out, network);  // summarize
+
+                    if (numNetworks > burnin) {
+                        printSummary (network, out);
+                    }
+                    if (numNetworks % 10000 == 0) progressStream.print(".");
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.err.println(numNetworks + " networks totally, " + burnin + " discarded as burn-in.");
+        progressStream.println("\nParsed " + numNetworks + " networks totally, " + burnin + " discarded as burn-in.");
+
+        out.close();
     }
 
-    private double getGammaProb(Network network) {
-        if (network.getReticulationNodeCount() != 1)
-            return Double.NaN;
+    private void printSummary (Network network, PrintStream out){
+        double rootHeight = network.getRoot().getHeight();
+        double originHeight = network.getOrigin().getHeight();
+        double length = network.getNetworkLength() - (originHeight - rootHeight);
 
-        NetworkNode tipA = null, tipB = null, tipC = null;
-        for (NetworkNode tip: network.getLeafNodes()) {
-            switch (tip.getLabel()) {
-                case "A":  tipA = tip;  break;
-                case "B":  tipB = tip;  break;
-                case "C":  tipC = tip;  break;
-                default:
-            }
-        }
-        if (tipA == null || tipB == null || tipC == null)
-            throw new RuntimeException("Check tip label!");
-
-        NetworkNode parentA = tipA.getParentByBranch(tipA.gammaBranchNumber);
-        NetworkNode parentB = tipB.getParentByBranch(tipB.gammaBranchNumber);
-        NetworkNode parentC = tipC.getParentByBranch(tipC.gammaBranchNumber);
-        if (parentB.isReticulation() && parentA.getChildren().contains(parentB) && parentC.getChildren().contains(parentB)) {
-            // System.out.println(parentB.getHeight() + "\t" + parentA.getHeight() + "\t" + parentC.getHeight()  + "\t" +
-            //                    network.getRoot().getHeight() + "\t" + network.getOrigin().getHeight());
-            if (parentB.getParentByBranch(parentB.gammaBranchNumber) == parentA)
-                return parentB.getGammaProb();
-            else
-                return 1.0 - parentB.getGammaProb();
-        }
-        else return Double.NaN;
-    }
-
-    /**
-     * print some summary statics, including
-     * 1. number of hybridization
-     * 2. network length
-     * 3. root height
-     * 4. time of youngest hybridization
-     * 5. gamma (NA if not the true network)
-     */
-    private void printSummary(PrintStream out, Network network) {
-        final int nHybrid = network.getReticulationNodeCount();
-        final double length = network.getNetworkLength();
-        final double height = network.getRoot().getHeight();
-        final double gamma = getGammaProb(network);
+        int nHybrid = network.getReticulationNodeCount();
 
         double tHybrid = 0.0;
         for (NetworkNode hNode: network.getReticulationNodes()) {
@@ -132,7 +106,83 @@ public class SummarizeNetwork extends Runnable {
                 tHybrid = hNode.getHeight();
         }
 
-        DecimalFormat df = new DecimalFormat("#.########");
-        out.println(nHybrid + "\t" + df.format(length) + "\t" + df.format(height) + "\t" + df.format(tHybrid) + "\t" + gamma);
+        out.println(length + "\t" + rootHeight + "\t" + nHybrid + "\t" + tHybrid);
+    }
+
+    private double calculateMean(List<Double> sample) {
+        double avg = 0.0;
+        int n = 1;
+        for (Double v : sample){
+            avg += (v - avg) / n;
+            n++;
+        }  // more numerically stable
+        return avg;
+    }
+
+    private double calculateMedian(List<Double> sample) {
+        final int length = sample.size();
+        int[] indices = new int[length];
+        HeapSort.sort(sample, indices);
+
+        int pos = length / 2;
+        if (length % 2 == 1) {
+            return sample.get(indices[pos]);
+        } else {
+            return (sample.get(indices[pos - 1]) + sample.get(indices[pos])) / 2.0;
+        }
+    }
+
+    private double[] calculateHPDInterval(double prop, List<Double> sample) {
+        final int length = sample.size();
+        int[] indices = new int[length];
+        HeapSort.sort(sample, indices);
+
+        double minRange = Double.MAX_VALUE;
+        int hpdIndex = 0;
+
+        int diff = (int) Math.round(prop * length);
+        for (int i = 0; i <= (length - diff); i++) {
+            double minValue = sample.get(indices[i]);
+            double maxValue = sample.get(indices[i + diff - 1]);
+            double range = Math.abs(maxValue - minValue);
+            if (range < minRange) {
+                minRange = range;
+                hpdIndex = i;
+            }
+        }
+        double lower = sample.get(indices[hpdIndex]);
+        double upper = sample.get(indices[hpdIndex + diff - 1]);
+
+        return new double[]{lower, upper};
+    }
+
+    private double[] calculateRange(List<Double> sample) {
+        double min = sample.get(0);
+        double max = min;
+        for (Double v : sample) {
+            if (v > max) max = v;
+            if (v < min) min = v;
+        }
+        return new double[]{min, max};
+    }
+
+    // 1st and 3rd quartiles
+    private double[] calculateQuartiles(List<Double> sample) {
+        final int length = sample.size();
+        int[] indices = new int[length];
+        HeapSort.sort(sample, indices);
+
+        int mid = length / 2;
+        int pos = mid / 2;
+        double first, third;
+        if (mid % 2 == 1 || length == 1) {
+            first = sample.get(indices[pos]);
+            third = sample.get(indices[length - pos - 1]);
+        } else {
+            first = (sample.get(indices[pos - 1]) + sample.get(indices[pos])) / 2.0;
+            third = (sample.get(indices[length - pos - 1]) + sample.get(indices[length - pos])) / 2.0;
+        }
+
+        return new double[]{first, third};
     }
 }
