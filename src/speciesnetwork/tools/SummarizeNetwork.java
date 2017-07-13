e142b866a513e6c4360921718481cdb86d56a7a2
Chi Zhang <zhangchicool@gmail.com>
2017-07-13 11:06:21 +0200

    for my own purpose

--- src/speciesnetwork/tools/SummarizeNetwork.java
+++ src/speciesnetwork/tools/SummarizeNetwork.java
@@ -65,8 +65,7 @@ public class SummarizeNetwork extends Runnable {
         final int burnin = burninInput.get();
 
         progressStream.print("Parsing network samples ");
-        // print header
-        out.println("length  height  nHybrid  tHybrid");
+        List<Network> networks = new ArrayList<>();
         int numNetworks = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
             String line;
@@ -80,7 +79,7 @@ public class SummarizeNetwork extends Runnable {
                     numNetworks++;
 
                     if (numNetworks > burnin) {
-                        printSummary (network, out);
+                        networks.add(network);
                     }
                     if (numNetworks % 10000 == 0) progressStream.print(".");
                 }
@@ -90,23 +89,250 @@ public class SummarizeNetwork extends Runnable {
         }
         progressStream.println("\nParsed " + numNetworks + " networks totally, " + burnin + " discarded as burn-in.");
 
+        printSummary_h2(networks, out);
+
         out.close();
     }
 
-    private void printSummary (Network network, PrintStream out){
-        double rootHeight = network.getRoot().getHeight();
-        double originHeight = network.getOrigin().getHeight();
-        double length = network.getNetworkLength() - (originHeight - rootHeight);
+    private void printSummary_net (List<Network> networks, PrintStream out){
+        // print header
+        out.println("length  height  nHybrid  tHybrid");
 
-        int nHybrid = network.getReticulationNodeCount();
+        for (Network network : networks) {
+            double rootHeight = network.getRoot().getHeight();
+            double originHeight = network.getOrigin().getHeight();
+            double length = network.getNetworkLength() - (originHeight - rootHeight);
 
-        double tHybrid = 0.0;
-        for (NetworkNode hNode: network.getReticulationNodes()) {
-            if (tHybrid == 0.0 || tHybrid > hNode.getHeight())
-                tHybrid = hNode.getHeight();
+            int nHybrid = network.getReticulationNodeCount();
+
+            double tHybrid = 0.0;  // youngest hybridization time
+            for (NetworkNode hNode : network.getReticulationNodes()) {
+                if (tHybrid == 0.0 || tHybrid > hNode.getHeight())
+                    tHybrid = hNode.getHeight();
+            }
+
+            out.println(length + "\t" + rootHeight + "\t" + nHybrid + "\t" + tHybrid);
+        }
+    }
+
+    private void printSummary_h1 (List<Network> networks, PrintStream out){
+        List<Double> gamma1 = new ArrayList<>();
+        List<Double> hgt_H1 = new ArrayList<>();
+        List<Double> height = new ArrayList<>();
+        List<Double> length = new ArrayList<>();
+
+        int nTrees = 0;
+        int nTruth = 0;
+        for (Network network : networks) {
+            // network height and length
+            double rootHeight = network.getRoot().getHeight();
+            double originHeight = network.getOrigin().getHeight();
+            double netLength = network.getNetworkLength() - (originHeight - rootHeight);
+            height.add(rootHeight);
+            length.add(netLength);
+
+            NetworkNode tipA = null, tipB = null, tipC = null;
+            for (NetworkNode tip: network.getLeafNodes()) {
+                switch (tip.getLabel()) {
+                    case "A":  tipA = tip;  break;
+                    case "B":  tipB = tip;  break;
+                    case "C":  tipC = tip;  break;
+                    default:
+                }
+            }
+            if (tipA == null || tipB == null || tipC == null)
+                throw new RuntimeException("Check tip label!");
+
+            // tree
+            if (network.getReticulationNodeCount() == 0)
+                nTrees++;
+
+            // true network
+            NetworkNode parentA = tipA.getParentByBranch(tipA.gammaBranchNumber);
+            NetworkNode parentB = tipB.getParentByBranch(tipB.gammaBranchNumber);
+            NetworkNode parentC = tipC.getParentByBranch(tipC.gammaBranchNumber);
+            if (network.getReticulationNodeCount() == 1 && parentB.isReticulation() &&
+                    parentA.getChildren().contains(parentB) && parentC.getChildren().contains(parentB)) {
+                nTruth++;
+
+                if (parentB.getParentByBranch(parentB.gammaBranchNumber) == parentA)
+                    gamma1.add(parentB.getGammaProb());
+                else
+                    gamma1.add(1.0 - parentB.getGammaProb());
+                hgt_H1.add(parentB.getHeight());
+            }
         }
 
-        out.println(length + "\t" + rootHeight + "\t" + nHybrid + "\t" + tHybrid);
+        // percentage of trees
+        final int nNetworks = networks.size();
+        out.print((double)nTrees/nNetworks + "\t");
+        // percentage of true networks
+        out.print((double)nTruth/nNetworks + "\t");
+
+        // network height
+        double median = calculateMedian(height);
+        double[] hpd = calculateHPDInterval(0.95, height);
+        out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+
+        // network length
+        median = calculateMedian(length);
+        hpd = calculateHPDInterval(0.95, length);
+        out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+
+        // gamma1 if true network
+        if (gamma1.size() == 0)
+            out.print(Double.NaN + "\t" + Double.NaN + "\t" + Double.NaN + "\t");
+        else {
+            median = calculateMedian(gamma1);
+            hpd = calculateHPDInterval(0.95, gamma1);
+            out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+        }
+
+        // h1 height if true network
+        if (hgt_H1.size() == 0)
+            out.print(Double.NaN + "\t" + Double.NaN + "\t" + Double.NaN + "\n");
+        else {
+            median = calculateMedian(hgt_H1);
+            hpd = calculateHPDInterval(0.95, hgt_H1);
+            out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\n");
+        }
+    }
+
+    private void printSummary_h2 (List<Network> networks, PrintStream out){
+        List<Double> gamma1 = new ArrayList<>();
+        List<Double> hgt_H1 = new ArrayList<>();
+        List<Double> gamma2 = new ArrayList<>();
+        List<Double> hgt_H2 = new ArrayList<>();
+        List<Double> height = new ArrayList<>();
+        List<Double> length = new ArrayList<>();
+
+        int nTrees = 0;
+        int nTruth = 0;  // number of true networks
+        int nOneHy = 0;  // number of networks with 1 hybridization
+        int nBCDHy = 0;  // number of networks with 1 hybridization and the BCDH structure
+        for (Network network : networks) {
+            // network height and length
+            double rootHeight = network.getRoot().getHeight();
+            double originHeight = network.getOrigin().getHeight();
+            double netLength = network.getNetworkLength() - (originHeight - rootHeight);
+            height.add(rootHeight);
+            length.add(netLength);
+
+            NetworkNode tipA = null, tipB = null, tipC = null, tipD = null;
+            for (NetworkNode tip: network.getLeafNodes()) {
+                switch (tip.getLabel()) {
+                    case "A":  tipA = tip;  break;
+                    case "B":  tipB = tip;  break;
+                    case "C":  tipC = tip;  break;
+                    case "D":  tipD = tip;  break;
+                    default:
+                }
+            }
+            if (tipA == null || tipB == null || tipC == null || tipD == null)
+                throw new RuntimeException("Check tip label!");
+
+            // tree
+            if (network.getReticulationNodeCount() == 0)
+                nTrees++;
+
+            // network with 1 hybridization
+            if (network.getReticulationNodeCount() == 1)
+                nOneHy++;
+
+            NetworkNode parentA = tipA.getParentByBranch(tipA.gammaBranchNumber);  // S1
+            NetworkNode parentB = tipB.getParentByBranch(tipB.gammaBranchNumber);  // S3
+            NetworkNode parentC = tipC.getParentByBranch(tipC.gammaBranchNumber);  // H2
+            NetworkNode parentD = tipD.getParentByBranch(tipD.gammaBranchNumber);  // S4
+            NetworkNode parentParentB = parentB.getParentByBranch(parentB.gammaBranchNumber);  // H1
+            NetworkNode parentParentD = parentD.getParentByBranch(parentD.gammaBranchNumber);  // S2
+
+            // networks with the BCDH structure
+            if (parentC.isReticulation() && parentB.getChildren().contains(parentC) && parentD.getChildren().contains(parentC)) {
+                if (network.getReticulationNodeCount() == 1) {
+                    // networks with 1 hybridization
+                    nBCDHy++;
+
+                    if (parentC.getParentByBranch(parentC.gammaBranchNumber) == parentB)
+                        gamma2.add(parentC.getGammaProb());
+                    else
+                        gamma2.add(1.0 - parentC.getGammaProb());
+                    hgt_H2.add(parentC.getHeight());
+                }
+                else if (network.getReticulationNodeCount() == 2 && parentParentB.isReticulation() &&
+                        parentA.getChildren().contains(parentParentB) && parentParentD.getChildren().contains(parentParentB)) {
+                    // true networks
+                    nTruth++;
+
+                    if (parentParentB.getParentByBranch(parentParentB.gammaBranchNumber) == parentA)
+                        gamma1.add(parentParentB.getGammaProb());
+                    else
+                        gamma1.add(1.0 - parentParentB.getGammaProb());
+                    hgt_H1.add(parentParentB.getHeight());
+
+                    if (parentC.getParentByBranch(parentC.gammaBranchNumber) == parentB)
+                        gamma2.add(parentC.getGammaProb());
+                    else
+                        gamma2.add(1.0 - parentC.getGammaProb());
+                    hgt_H2.add(parentC.getHeight());
+                }
+            }
+        }
+
+        // percentage of trees
+        final int nNetworks = networks.size();
+        out.print((double)nTrees/nNetworks + "\t");
+        // percentage of true networks
+        out.print((double)nTruth/nNetworks + "\t");
+        // percentage of 1 hybridization
+        out.print((double)nOneHy/nNetworks + "\t");
+        // percentage of 1 hybridization and BCDH
+        out.print((double)nBCDHy/nNetworks + "\t");
+
+        // network height
+        double median = calculateMedian(height);
+        double[] hpd = calculateHPDInterval(0.95, height);
+        out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+
+        // network length
+        median = calculateMedian(length);
+        hpd = calculateHPDInterval(0.95, length);
+        out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+
+        // gamma1 if true network
+        if (gamma1.size() == 0)
+            out.print(Double.NaN + "\t" + Double.NaN + "\t" + Double.NaN + "\t");
+        else {
+            median = calculateMedian(gamma1);
+            hpd = calculateHPDInterval(0.95, gamma1);
+            out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+        }
+
+        // h1 height if true network
+        if (hgt_H1.size() == 0)
+            out.print(Double.NaN + "\t" + Double.NaN + "\t" + Double.NaN + "\t");
+        else {
+            median = calculateMedian(hgt_H1);
+            hpd = calculateHPDInterval(0.95, hgt_H1);
+            out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+        }
+
+        // gamma2 if BCDH structure
+        if (gamma2.size() == 0)
+            out.print(Double.NaN + "\t" + Double.NaN + "\t" + Double.NaN + "\t");
+        else {
+            median = calculateMedian(gamma2);
+            hpd = calculateHPDInterval(0.95, gamma2);
+            out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\t");
+        }
+
+        // h2 height if BCDH structure
+        if (hgt_H2.size() == 0)
+            out.print(Double.NaN + "\t" + Double.NaN + "\t" + Double.NaN + "\n");
+        else {
+            median = calculateMedian(hgt_H2);
+            hpd = calculateHPDInterval(0.95, hgt_H2);
+            out.print(median + "\t" + hpd[0] + "\t" + hpd[1] + "\n");
+        }
     }
 
     private double calculateMean(List<Double> sample) {
@@ -154,35 +380,5 @@ public class SummarizeNetwork extends Runnable {
         double upper = sample.get(indices[hpdIndex + diff - 1]);
 
         return new double[]{lower, upper};
-    }
-
-    private double[] calculateRange(List<Double> sample) {
-        double min = sample.get(0);
-        double max = min;
-        for (Double v : sample) {
-            if (v > max) max = v;
-            if (v < min) min = v;
-        }
-        return new double[]{min, max};
-    }
-
-    // 1st and 3rd quartiles
-    private double[] calculateQuartiles(List<Double> sample) {
-        final int length = sample.size();
-        int[] indices = new int[length];
-        HeapSort.sort(sample, indices);
-
-        int mid = length / 2;
-        int pos = mid / 2;
-        double first, third;
-        if (mid % 2 == 1 || length == 1) {
-            first = sample.get(indices[pos]);
-            third = sample.get(indices[length - pos - 1]);
-        } else {
-            first = (sample.get(indices[pos - 1]) + sample.get(indices[pos])) / 2.0;
-            third = (sample.get(indices[length - pos - 1]) + sample.get(indices[length - pos])) / 2.0;
-        }
-
-        return new double[]{first, third};
     }
 }
